4ec48fa8e42b1e16fb2d355155b221a4c12d32cf
GIgaBYte6610 <bhattagaurav100@gmail.com>
2026-05-13 17:15:25 +0545

    fix: black formatting on base.py

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -231,4 +231,4 @@ else:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
